bdc29afa35e9142f162d03da1ecc1671b05a5512
Abhinav Gupta <abhi_cgupta@yahoo.com>
2013-12-01 05:36:34 +0000

    read output from index.html

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var app = express();
+var fs =require('fs');
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var web = fs.readFileSync('index.html');
+  var str = web.toString(web);
+  response.send(str);
+  //sponse.send('Hello World 2!');
 });
 
 var port = process.env.PORT || 5000;
